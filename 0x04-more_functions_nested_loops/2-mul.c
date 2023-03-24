934ebd24d6fbe30082960434a29af49bde0e2cb2
gloxoss <gloxoss@gmail.com>
2023-03-24 05:42:30 -0700

    h

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-  * _isdigit - Check if it's a digit
+  * mul - multiples two ints
   * @a: int
   * @b: int
-  * Return: 1 if true, 0 if false
+  * Return: always 0
   */
 
 int mul(int a, int b)
