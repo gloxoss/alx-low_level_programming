cafd0ba02312a3ca384e22391b9fddbc8b859f82
gloxoss <gloxoss@gmail.com>
2023-03-30 09:02:50 -0700

    h

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,18 +2,17 @@
 
 /**
  * print_number - prints an integer using _putchar
- * @n: the integer to print
+ * @n: integer to be printed
  */
 
 void print_number(int n)
 {
-if (n < 0) {
+if (n < 0)
+{
 _putchar('-');
 n = -n;
 }
-if (n / 10) {
+if (n / 10)
 print_number(n / 10);
+_putchar((n % 10) + '0');
 }
-_putchar(n % 10 + '0');
-}
-
