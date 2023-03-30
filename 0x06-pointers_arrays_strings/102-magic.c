96f60577a4615c79e241b4c0249723d60ae2c867
gloxoss <gloxoss@gmail.com>
2023-03-30 09:19:37 -0700

    h

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,18 +2,14 @@
 
 int main(void)
 {
-int n;
+  int n;
+  int a[5];
+  int *p;
 
-int a[5];
-
-int *p;
-
-a[2] = 1024;
-
-p = &n;
-
-*((int *)(((char *)p) + 2 * sizeof(int))) = 98;
-
-printf("a[2] = %d\n", a[2]);
-return (0);
+  a[2] = 1024;
+  p = &n;
+  *((int *)(((char *)p) + 2 * sizeof(int))) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
