5efdcd614c6529f76f9aa794c9145f713bf47074
gloxoss <gloxoss@gmail.com>
2023-03-30 15:26:23 -0700

    h

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,54 +1,47 @@
-#include <string.h>
-#inc
-/**
-* infinite_add - adds two numbers
-* @n1: first number
-* * @n2: second number
-* @r: buffer for result
-* @size_r: buffer size
-* ahhh! Crazy task! Expand your knowledge
-* Return: address of r or 0
-*/
+#include "main.h"
 
+/**
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
+ * @size_r: buffer size
+ * ahhh! Crazy task! Expand your knowledge
+ * Return: address of r or 0
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int len1, len2, i, j, k, sum, carry;
+	int i, j, k, l, m, n;
 
-len1 = strlen(n1);
-
-len2 = strlen(n2);
-carry = 0;
-k = 0;
-
-for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 || carry; i--, j--, k++)
-{
-sum = carry;
-
-if (i >= 0)
-sum += n1[i] - '0';
-
-if (j >= 0)
-sum += n2[j] - '0';
-
-if (k >= size_r - 1 || sum > 9)
-return (0);
-
-carry = sum / 10;
-
-r[k] = sum % 10 + '0';
-}
-
-r[k] = '\0';
-
-for (i = 0, j = k - 1; i < j; i++, j--)
-{
-char tmp = r[i];
-
-r[i] = r[j];
-
-r[j] = tmp;
-
-}
-
-return (r);
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
+		return (0);
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	{
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
+	}
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
+	return (r);
 }
