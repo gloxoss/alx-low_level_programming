6f178ae306e1b396bcd0286c2c60da018d6cbe89
gloxoss <gloxoss@gmail.com>
2023-03-29 19:22:14 -0700

    h

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,30 +6,22 @@
  * @s1: Pointer to the first string to be compared.
  * @s2: Pointer to the second string to be compared.
  *
- * Return: Integer value indicating the lexicographical relationship between
- *         the two strings. If s1 is less than s2, the function returns a
- *         negative number; if s1 is greater than s2, the function returns a
- *         positive number; and if s1 and s2 are equal, the function returns 0.
+ * Return:value less than 0 if string is less than the other,
+ * value greater than 0
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int counter, compare_value;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	counter = 0;
+
+	while (s1[counter] == s2[counter] && s1[counter] != '\0')
 	{
-		if (s1[i] != s2[i])
-		{
-			return ((s1[i] < s2[i]) ? -15 : 15);
-		}
+		counter++;
 	}
-	if (s1[i] == '\0' && s2[i] == '\0')
-	{
-		return (0);
-	}
-	else
-	{
-		return ((s1[i] < s2[i]) ? -15 : 15);
-	}
+	compare _value = s1[counter] - s2[counter];
+
+	return (compare_value);
 }
 
