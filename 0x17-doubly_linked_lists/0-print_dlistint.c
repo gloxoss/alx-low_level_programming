b6f0b6876992b21df5da38b03d31a845e96a9ac0
gloxoss <112545439+gloxoss@users.noreply.github.com>
2023-06-16 00:29:54 +0100

    Add files via upload

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include "lists.h"
+
+/**
+ * print_dlistint - printing data from doubly linked list
+ * @h: the head
+ * Return: the size of the list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *curr = h;
+	size_t size = 0;
+
+	if (!curr)
+		return (0);
+	while (curr)
+	{
+		printf("%d\n", curr->n);
+		curr = curr->next;
+		size++;
+	}
+	return (size);
+}
